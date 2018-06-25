e13bc2a03d8373dbe0edbbe15a7b41fbb847c38c
Jeremy Parker <jrobinparker@gmail.com>
2018-06-25 18:16:58 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,9 @@ end
 
 def input_to_index(user_input)
   user_input = gets.chomp!
-  user_input.to_i
+  user_input = user_input.to_i
+  user_input = user_input - 1
+  return user_input
 end 
 
 def move(board, user_input, user_char = "X")
